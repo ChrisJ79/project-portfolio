227bd65420806f7478e614ec32a01c165b9b3668
Chris <csjohnson79@icloud.com>
2019-11-20 20:46:31 -0700

    installed vue-cli, experimented with app on localhost 8080

---
+++ vue-proj/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <skills />
+  </div>
+</template>
+
+<script>
+import skills from './components/skills.vue'
+
+export default {
+  name: 'app',
+  components: {
+    skills
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
